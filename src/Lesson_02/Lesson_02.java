be2e544a0860d6f1e915bff403c15dd20cf38c68
denprikh <dprihodko@meta-devs.io>
2022-01-21 17:12:45 +0300

    2-nd Lesson added

---
+++ src/Lesson_02/Lesson_02.java
@@ -0,0 +1,56 @@
+package Lesson_02;
+
+import java.util.Scanner;
+
+public class Lesson_02 {
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 8;
+        sum(a, b);
+        sum(3, a);
+        sum(5, 23);
+
+        boolean aGreaterThenB = a > b;
+        boolean aLessThenB = a < b;
+        boolean aGreaterOrEqualThenB = a >= b;
+        boolean aLessOrEqualThenB = a <= b;
+
+        if (aGreaterThenB) {
+            System.out.println("a > b");
+        } else {
+            System.out.println("False");
+        }
+
+        if (aLessThenB) {
+            System.out.println("a < b");
+        }else {
+            System.out.println("False");
+        }
+        if (aGreaterOrEqualThenB) {
+            System.out.println("a >= b");
+        }else {
+            System.out.println("False");
+        }
+        if (aLessOrEqualThenB) {
+            System.out.println("a <= b");
+        } else {
+            System.out.println("False");
+        }
+        System.out.println("-------Задание-------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int num = scanner.nextInt();
+
+        if (num%2 == 0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
+    }
+
+    static void sum(int x, int y) {
+        int z = (x + y);
+        System.out.println(z);
+    }
+}
