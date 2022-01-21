42eec55bcef59bf9d54538952dcc1def356f6824
denprikh <dprihodko@meta-devs.io>
2022-01-21 17:15:38 +0300

    4-th Lesson added
    Renamed Class
    Added Ctrl+Alt+L
    
    Deleted comments
    
    Combined all commits

---
+++ src/Lesson_04/LessonFour.java
@@ -0,0 +1,25 @@
+package Lesson_04;
+
+public class LessonFour {
+    public static void main(String[] args) {
+        int year = 1980;
+        for (int j = 0; j < 6; j++) {
+            System.out.println("Год " + year++);
+        }
+
+        int[] arr1 = new int[5];
+        System.out.println(arr1[4]);
+        arr1[3] = 67;
+        for (int j = 0; j < arr1.length; j++) {
+            System.out.println(arr1[j]);
+        }
+
+        int[] arr2 = new int[]{1, 2, 3, 4, 5};
+        arr2[3] = 67;
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
+        }
+        for (int item : arr2)
+            System.out.println(item);
+    }
+}
