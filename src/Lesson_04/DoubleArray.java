27933dcab118f107f7cef30fa006b42bc215e7f3
denprikh <dprihodko@meta-devs.io>
2022-01-25 20:40:42 +0300

    Renamed Class Added Ctrl+Alt+L

--- src/Lesson_04/DoubleArray.java
+++ src/Lesson_04/DoubleArray.java
@@ -7,7 +7,7 @@ public class DoubleArray {
         array[0][0] = 5;
         array[0][1] = 55;
 
-        System.out.println( array[0][1] + array[0][0] );
-        System.out.println( array[1][0] + array[0][0] );
+        System.out.println(array[0][1] + array[0][0]);
+        System.out.println(array[1][0] + array[0][0]);
     }
 }
--- src/Lesson_04/LessonFour.java
+++ src/Lesson_04/LessonFour.java
@@ -1,6 +1,6 @@
 package Lesson_04;
 
-public class Lesson_04 {
+public class LessonFour {
     public static void main(String[] args) {
         // 1980 - 1985 - выводить, какой это год
         int year = 1980;
@@ -8,21 +8,21 @@ public class Lesson_04 {
             System.out.println("Год " + year++);
         }
 
-        int [] arr1 = new int[5];
-        System.out.println( arr1[4] );
+        int[] arr1 = new int[5];
+        System.out.println(arr1[4]);
         arr1[3] = 67;
         for (int j = 0; j < arr1.length; j++) {
-            System.out.println( arr1[j] );
+            System.out.println(arr1[j]);
         }
 
-        int [] arr2 = new int[]{1, 2, 3, 4, 5};
+        int[] arr2 = new int[]{1, 2, 3, 4, 5};
         arr2[3] = 67;
 //        Вывод массива через цикл for
         for (int i = 0; i < arr2.length; i++) {
             System.out.println(arr2[i]);
         }
 //        Вывод через оператор foreach
-        for (int item: arr2)
+        for (int item : arr2)
             System.out.println(item);
     }
 }
